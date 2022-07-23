4f5aac7ca47cac5a7766d28d7a63f248bdfc8788
carlosrodriguez120 <caralbertrosi@gmail.com>
2022-07-23 17:20:08 -0500

    commit de guardado dia 4 funciones iterativas

--- main.py
+++ main.py
@@ -98,7 +98,7 @@
 # sorteo = s3.pop()
 # print(sorteo)
 
-#boleanos----------------------------------------------------------------------
+# boleanos----------------------------------------------------------------------
 
 # var1=True
 # var2=False
@@ -130,35 +130,135 @@ proyecto analizador de texto
 en python
 
 """
+#
+# texto=str(input("Por favor ingrese su texto"))
+# letra1=input("ingrese la primer letra a buscar")
+# letra2=input("ingrese la segunda letra a buscar")
+# letra3=input("ingrese la tercera letra a buscar")
+#
+# texto = texto.lower()
+#
+# letra1 = letra1.lower()
+# letra2 = letra2.lower()
+# letra3 = letra3.lower()
+#
+#
+#
+# print(f"la letra {letra1} aparece {texto.count(letra1)} veces en  el texto")
+# print(f"la letra {letra2} aparece {texto.count(letra2)} veces en  el texto")
+# print(f"la letra {letra3} aparece {texto.count(letra3)} veces en  el texto")
+#
+# result = len(texto.split())
+# print(f"el texto tiene {result} palabras")
+#
+# print(f"la primer letra del texto es {texto[0]}")
+# print(f"la ultima letra del texto es {texto[-1]}")
+#
+# lista =texto.split()
+# print(texto.split())
+#
+#
+# print(lista[::-1])
+# print((" ").join(lista[::-1]))
+#
+# print("python" in texto)
 
-texto=str(input("Por favor ingrese su texto"))
-letra1=input("ingrese la primer letra a buscar")
-letra2=input("ingrese la segunda letra a buscar")
-letra3=input("ingrese la tercera letra a buscar")
+# operadores logicos-----------------------------------------------------------------------------------------------------
+# num1 = 36
+# num2 = 17
+# mi_bool  = num1>=num2
+#
+# and
+# or
+# not
 
-texto = texto.lower()
+# estructuras  de control if ----------------------------------------------------------------------------------------------
 
-letra1 = letra1.lower()
-letra2 = letra2.lower()
-letra3 = letra3.lower()
+# num1 = input("Ingresa un número:")
+# num2 = input("Ingresa otro número:")
+#
+# f"{num1} es mayor que {num2}"
+# "num2 es mayor que num1"
+# "num1 y num2 son iguales"
+#
+# if num2 < num1:
+#     print(f"{num1} es mayor que {num2}")
+# elif num1 < num2:
+#     print(f"{num2} es mayor que {num1}")
+# else:
+#     print(f"{num1} y {num2} son iguales")
+
+# for-----------------------------------------------------------------------------------
+# lista = ["1", "2", "3", "4", "6", "7",]
+#
+# for letra in lista:
+#     print(f"letra {letra} y va en el espacio {lista.index(letra)}")
 
 
+# lista = ["andres", "pablo","juan","pedro","laura","andres"]
+# for nombre in lista:
+#     if nombre.startswith("p"):
+#         print(nombre)
+#
+#
+# lista2 = [1,2,3,4,5,6,7]
+# valor = 0
+#
+# for n in lista2:
+#     valor=valor+n
+#
+# print(valor)
 
-print(f"la letra {letra1} aparece {texto.count(letra1)} veces en  el texto")
-print(f"la letra {letra2} aparece {texto.count(letra2)} veces en  el texto")
-print(f"la letra {letra3} aparece {texto.count(letra3)} veces en  el texto")
 
-result = len(texto.split())
-print(f"el texto tiene {result} palabras")
+# arreglo = [[1,2,3,4,5],[1,2,3,4,5],[5,4,3,2,1]]
+# for a,b,c,d,e in arreglo:
+#     print(a)
+#     print(b)
+#     print(c)
+#     print(d)
+#     print(e)
 
-print(f"la primer letra del texto es {texto[0]}")
-print(f"la ultima letra del texto es {texto[-1]}")
-
-lista =texto.split()
-print(texto.split())
+# alumnos_clase = ["María", "José", "Carlos", "Martina", "Isabel", "Tomás", "Daniela"]
+# for nombre in alumnos_clase:
+#     print(f"Hola {nombre}")
 
 
-print(lista[::-1])
-print((" ").join(lista[::-1]))
+# lista_numeros = [1, 5, 8, 7, 6, 8, 2, 5, 2, 6, 4, 8, 5, 9, 8, 3, 5, 4, 2, 5, 6, 4]
+# suma_numeros = 0
+#
+# for n in lista_numeros:
+#     suma_numeros = suma_numeros + n
+#
+# print(suma_numeros)
 
-print("python" in texto)
+# lista_numeros = [1, 5, 8, 7, 6, 8, 2, 5, 2, 6, 4, 8, 5, 9, 8, 3, 5, 4, 2, 5, 6, 4]
+# suma_pares = 0
+# suma_impares = 0
+#
+# for n in lista_numeros:
+#     if n % 2 == 0:
+#         suma_pares = suma_pares + n
+#     else:
+#         suma_impares = suma_impares + n
+#
+# print(f"numeros pares{suma_pares}")
+# print(f"numeros pares{suma_impares}")
+
+# for -------------------------------------------------------------------------------------
+#
+# moneda = 5
+# while moneda >=0:
+#     print(f"tengo {moneda} monedas")
+#     moneda -= 1
+# else:
+#     print("bye")
+#
+
+# range-----------------------------------------------------------------------------------
+# lista = list(range(2,101,2))
+# print(lista)
+
+# enumerate-------------------------------------------------------------------------------
+lista = ["a", "b", "c", "d", "e"]
+for index, n in enumerate(lista):
+    print(index, n)
