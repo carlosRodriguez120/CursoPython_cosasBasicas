6ef382d28bdb5094fb9b008ee3c7bcdeee67c935
carlosrodriguez120 <caralbertrosi@gmail.com>
2022-07-23 11:57:11 -0500

    mensaje del primer commit para hacer push

---
+++ main.py
@@ -0,0 +1,99 @@
+# text = "este es el texto de carlos"
+# resultado = text.split(" ")
+# print(resultado)
+# a = "aprender"
+# b = "python"
+# c = "es"
+# d = "genial"
+#
+# e = " ".join([a, b, c, d])
+# print(e)
+# print(type(e))
+#
+#
+# print(e.find("es"))
+#
+#
+# print(e.replace("es","era"))
+#
+# h="hola"
+# print(h.center(100,"."))
+
+# frase = "Especialmente en las comunicaciones electrónicas, la escritura enteramente en mayúsculas equivale a gritar."
+# print((frase.upper()))
+#
+# lista_palabras = ["La","legibilidad","cuenta."]
+# frase = frase.join([lista_palabras])
+# print(frase)
+
+# n1 = "kari"
+# n2 = "hi"
+#
+# print(n1 * 10)
+# poema = """
+# hola
+# amigo
+# como
+# estas
+# """
+#
+# print(poema.center(100,"."))
+#
+# text="""Tierra mojada
+# mis recuerdos de viaje,
+# entre las lluvias"""
+#
+# print("agua" in text)
+#
+# palabra ="electroencefalografista"
+# print(len(palabra))
+
+
+# listas--------------------------------------------------------------
+
+# miLista=["g","b","o","d","c","a"]
+# print(type(miLista))
+# print(len(miLista))
+# print(miLista[3])
+# miLista.append("e")
+# miLista.pop(4)
+# print(miLista)
+# miLista.sort()
+# print(miLista)
+#
+# medios_transporte = ["avión", "auto", "barco", "bicicleta"]
+# medios_transporte.append("motocicleta")
+# print(medios_transporte)
+
+# diccionarios---------------------------------------------------------
+
+# diccionario = {"c1":"valor de c1" , "c2":"valor de c2", "c3":"valor de c3"}
+# print((diccionario["c2"]))
+#
+# print(diccionario)
+# print(diccionario.values())
+# print(diccionario.keys())
+# print(diccionario.items())
+#
+# mi_dic = {"nombre": "Karen", "apellido":"Jurgens","edad":"35","ocupacion":"Periodista"}
+# print(mi_dic.values())
+
+
+# tuplas------------------------------------------------------------------
+
+# tupla=(1,2,3,4,(5,6,4,3,4,5,3),65,6,3)
+# print(tupla[4].index(4))
+
+# sets-------------------------------------------------------------------
+# miSet = set([1, 4, 3, (1, 2, 3), 5, 6])
+# miSet.add(0)
+# print(type(miSet))
+# print(len(miSet))
+#
+# set2 = {5, 7, 8, 9, 6}
+# s3 = miSet.union(set2)
+# print(s3)
+# miSet.remove((1,2,3))
+# print(miSet)
+# sorteo = s3.pop()
+# print(sorteo)
